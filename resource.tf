16ea945da89cbb4f15a3210ffdb5023758728595
Manoharan <kiruba729@gmail.com>
2021-01-30 18:20:57 +0530

    changed resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-04b1ddd35fd71475a"
+  ami                    = "ami-01aab85a5e4a5a0fe"
   instance_type          = "t2.micro"
   key_name               = "phpapplication"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
